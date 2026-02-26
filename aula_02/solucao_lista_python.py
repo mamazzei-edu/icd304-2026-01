0dbe6b40e459b20e3cab38ec7f86f7f2e476d532
Marco Mazzei <professor@didaticovirtual.com.br>
2026-02-26 12:23:44 -0300

    Exemplo de solução utilizando symbols

---
+++ aula_02/solucao_lista_python.py
@@ -0,0 +1,26 @@
+import sympy as sp
+
+x = sp.symbols('x')
+
+f = x**3
+g = 5
+h = x**2 + 3*x
+## uso de sp.sin - símbolo específico da biblioteca
+i = x**2 * sp.sin(x)
+j = x / (x+1)
+## uso de sp.sin - símbolo específico da biblioteca
+k = sp.sin(x**2)
+
+f_deriv = sp.diff(f,x)
+g_deriv = sp.diff(g,x)
+h_deriv = sp.diff(h,x)
+i_deriv = sp.diff(i,x)
+j_deriv = sp.diff(j,x)
+k_deriv = sp.diff(k,x)
+
+print(f"f'(x) = {f_deriv}")
+print(f"g'(x) = {g_deriv}")
+print(f"h'(x) = {h_deriv}")
+print(f"i'(x) = {i_deriv}")
+print(f"j'(x) = {j_deriv}")
+print(f"k'(x) = {k_deriv}")
